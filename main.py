f99e1615956178139ccfb3b2eb032b571530c7ab
marcelo <pioriam@gmail.com>
2026-01-05 15:00:03 -0300

    atualizacao

--- main.py
+++ main.py
@@ -2,19 +2,44 @@
 import streamlit as st
 import  pandas as pd
 import yfinance as yf
+from streamlit import columns
+
 
 @st.cache_data
-def carregar_dados(empresa):
-    dados_acao = yf.Ticker(empresa)
+def carregar_dados(empresas):
+    texto_tickers = " ".join(empresas)
+    dados_acao = yf.Tickers(texto_tickers)
     cotacao_acao = dados_acao.history(start = "2025-01-01", end = "2025-12-31")
-    cotacao_acao = cotacao_acao[["Close"]]
+    cotacao_acao = cotacao_acao["Close"]
+    print(cotacao_acao)
     return cotacao_acao
 
-dados = carregar_dados("ITUB4.SA")
+
+acoes = ["MGLU3.SA", "PETR4.SA", "ITUB4.SA", "VALE3.SA"]
+dados = carregar_dados(acoes)
 
 st.write("""
  # Preço de Ações
- O gráfico abaixo representa o preço das ações do Itaú ao longo dos anos.... 
+ O gráfico abaixo representa o preço das ações ao longo dos anos.... 
  """)
 
+st.sidebar.header("Filtros")
+
+lista_selecao = st.sidebar.multiselect("Escolha as ações para visualizar: ",dados.columns)
+
+data_ini =dados.index.min().to_pydatetime()
+data_fin = dados.index.max().to_pydatetime()
+intervalor= st.sidebar.slider("Selecione o periodo:" ,
+                  min_value=data_ini,
+                  max_value=data_fin,
+                  value=(data_ini,data_fin)
+                  )
+
+if lista_selecao:
+    dados = dados[lista_selecao]
+    if len(lista_selecao) == 1:
+        acao_unica = lista_selecao[0]
+        dados = dados.rename(columns={acao_unica:"Close"})
+
+dados = dados.loc[intervalor[0]:intervalor[1]]
 st.line_chart(dados)
